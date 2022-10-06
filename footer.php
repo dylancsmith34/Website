96846e57cff8da9157cc9e01c3e7464d8bbebd64
dylancsmith34 <115128175+dylancsmith34@users.noreply.github.com>
2022-10-05 23:06:58 -0500

    Footer

---
+++ footer.php
@@ -0,0 +1,13 @@
+<!-- Site footer -->
+    <html>
+    <head>
+    </head>
+    <body>
+        <center>
+            <ul id="Footer">
+                <a href="/cal_index.php">Calendar</a>
+                <a href="/adminLogin.php">Admin Portal</a>
+            </ul>
+        </center>
+    </body>
+    </html>
