a681e0b762b8e2f3268af93b3270df0203dadfb7
dylancsmith34 <115128175+dylancsmith34@users.noreply.github.com>
2022-10-05 23:08:47 -0500

    Header

---
+++ header.php
@@ -0,0 +1,24 @@
+<html>
+
+    <head>
+        <?php require('config.php');?>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styles.css">
+        <title>Merlot and a Masterpiece</title>
+        <link rel="shortcut icon" type="image/jpg" href="images/logo.png">
+    </head>
+
+    <body>
+        <div class="header">
+            <div class="header-image">
+                <a href="index.php"><img src="images/logoWithNameHorizontal.png" href="index.php" style="width:14%;"></a>
+                <div class="header-right">
+                    <a class="active" href="/index.php">Home</a>
+                    <a href="/cal_index.php">Calendar</a>
+                </div>
+            </div>
+        </div>
+    </body>
+
+
+    <html>
