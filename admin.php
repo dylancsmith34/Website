17cea0d8b3cf498362b536e266573f7256a92327
dylancsmith34 <115128175+dylancsmith34@users.noreply.github.com>
2022-10-05 23:28:14 -0500

    admin portal

--- admin.php
+++ admin.php
@@ -71,17 +71,4 @@ require('config.php');
         <div><?php require('footer.php');?></div>
     </body>
 </html>
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+
